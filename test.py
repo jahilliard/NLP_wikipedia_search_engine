48762d288a999f8d82f05446b66d2311231ede5e
jahilliard <justin.a.hilliard@gmail.com>
2016-05-28 11:07:55 -0700

    testing async load versus normal

--- test.py
+++ test.py
@@ -4,6 +4,10 @@ import csv
 import os.path
 import pandas as pd
 import numpy as np
+import asyncio
+import aiohttp
+
+
 
 file_name = 'category_list/category_list.csv'
 categories = []
@@ -27,18 +31,27 @@ def read_categories_to_load(file_name = 'category_list/category_list.csv'):
 		categories = ['Machine_learning', 'Business_software']
 	return categories
 
+async def load_page_async(session, url):
+	with aiohttp.Timeout(10):
+		async with session.get(url) as response:
+			assert response.status == 200
+			return await response.read()
+
 def load_categories_from_wikipedia(categories = ['Machine_learning', 'Business_software']):
 	temp =[]
 	for category in categories:
-		page = requests.get("https://en.wikipedia.org/wiki/Category:" + category).text
-		doc = html.fromstring(page)
-		for page in doc.get_element_by_id("mw-pages").find_class("mw-category-group"):
-			temp.append([n[2] for n in page.iterlinks()])
+		loop = asyncio.get_event_loop()
+		connector = aiohttp.TCPConnector(verify_ssl=False)
+		with aiohttp.ClientSession(loop=loop, connector=connector) as session:
+			category_html = loop.run_until_complete(
+				load_page_async(session, "https://en.wikipedia.org/wiki/Category:" + category))
+			doc = html.fromstring(category_html)
+			for page in doc.get_element_by_id("mw-pages").find_class("mw-category-group"):
+				temp.append([link_data[2] for link_data in page.iterlinks()])
+				# [loop.run_until_complete(load_page_async(session, link_data[2])) for link_data in page.iterlinks()]
 			print(temp)
 
-
 load_categories_from_wikipedia()
-
 
 
 
