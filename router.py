dfa69ef0ed0311a0ebf84e30f618b17a6e4c62b7
jahilliard <justin.a.hilliard@gmail.com>
2016-05-30 04:55:48 -0700

    set up testing framework

--- router.py
+++ router.py
@@ -7,11 +7,23 @@ def main_router(args):
 		db.build_db()
 		db.close_connection()
 
-	# For Dev purposes 
-	elif args[1] == "/drop_tabs":
-		print("drop_tabs")
-		db.drop_tables()
-		db.close_connection()
+	# TESTS: really basic tests For Dev purposes 
+	elif args[1] == "/tests":
+		try:
+			db.build_db()
+			print('\033[92m' + "	PASS:\033[94m DB Build pass\033[0m")
+		except:
+			print('\033[91m' + "	FAIL: DB Build fail\033[0m")
+		try:
+			db.drop_tables()
+			print('\033[92m' + "	PASS:\033[94m Drop DB tables pass\033[0m")
+		except:
+			print('\033[91m' + "	FAIL: DB Build fail\033[0m") 
+		try:
+			db.close_connection()
+			print('\033[92m' + "	PASS:\033[94m Close DB connection pass\033[0m")
+		except:
+			print('\033[91m' + "	FAIL: Close DB connection fail\033[0m") 
 		
 main_router(sys.argv)
 
